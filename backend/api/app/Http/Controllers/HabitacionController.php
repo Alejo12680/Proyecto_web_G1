608be60350458db52f9a0a58b398c89b3de51815
johanakarina <johana_karina@hotmail.com>
2021-10-04 02:14:27 -0500

    api

--- backend/api/app/Http/Controllers/HabitacionController.php
+++ backend/api/app/Http/Controllers/HabitacionController.php
@@ -22,4 +22,26 @@ class HabitacionController extends Controller
 
         // return $request->query('text');}
     }
+
+    public function filter(Request $request){
+        $filter = $request->input('filter');
+
+        switch($filter){
+            case 'piso':
+                $habitaciones = Habitacion::whereBetween('piso', [$request->input('min'), $request->input('max')])->get();
+                break;
+            case 'precio':
+                $habitaciones = Habitacion::whereBetween('precio', [$request->input('min'), $request->input('max')])->get();
+                break;
+            case 'capacidad':
+                $habitaciones = Habitacion::where('capacidad', $request->input('value'))->get();
+                break;
+            case 'estrellas':
+                $habitaciones = Habitacion::where('estrellas', $request->input('value'))->get();
+                break;
+        }
+
+        return $habitaciones;
+    }
+
 }
